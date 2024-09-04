147c359ddf6c1af36e3103172a9de62d2557a71c
Richard Goggin <richardgoggin@gmail.com>
2024-09-04 22:40:38 +0000

    Init my_blog

--- my_project/urls.py
+++ my_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from hello_world import views as index_views
+from my_blog import views as index_views
 
 urlpatterns = [
     path('', index_views.index, name='index'),
