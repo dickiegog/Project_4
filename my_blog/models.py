aa44aaa7007fccd396946a7d273817a3107c13d9
Richard Goggin <richardgoggin@gmail.com>
2024-09-21 22:50:14 +0000

    Implemented the Comment model with ForeignKey relationships

--- my_blog/models.py
+++ my_blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        'Post', on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
