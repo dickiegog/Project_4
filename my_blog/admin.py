ee9c38817bfaf0c06d4214c0e1b35fe0ac4a69d0
Richard Goggin <richardgoggin@gmail.com>
2024-09-22 13:09:53 +0000

    Update models.py and admin to include posts and comments

--- my_blog/admin.py
+++ my_blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Post, Comment  # Import your models
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
--- my_blog/models.py
+++ my_blog/models.py
@@ -1,12 +1,23 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
 
+# Post Model
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="posts")
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=(('Draft', 'Draft'), ('Published', 'Published')), default='Draft')
+
+    def __str__(self):
+        return self.title
+
+
+# Comment Model
 class Comment(models.Model):
     post = models.ForeignKey(
-        'Post', on_delete=models.CASCADE, related_name="comments")
+        Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
