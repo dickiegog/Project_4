0d48bd5ca824d7993ed006b95e1560d5830e657f
Richard Goggin <richardgoggin@gmail.com>
2024-09-22 20:38:07 +0000

    Add homepage view for posts

--- my_blog/views.py
+++ my_blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
-def index(request): 
-    return HttpResponse("Hello, world!")
+class PostList(generic.ListView):
+    queryset = Post.objects.all()  # Retrieves all the posts from the Post model
+    template_name = 'blog/post_list.html'  # Refers to the template file
