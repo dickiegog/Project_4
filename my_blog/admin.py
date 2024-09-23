2de5dcb2bc344c613cbcc3fddb0a1052ef0c916e
Richard Goggin <richardgoggin@gmail.com>
2024-09-23 23:16:55 +0000

    Reset database

--- my_blog/admin.py
+++ my_blog/admin.py
@@ -1,15 +1,1 @@
 from django.contrib import admin
-from .models import Post, Comment  # Import your models
-from django_summernote.admin import SummernoteModelAdmin
-
-@admin.register(Post)
-class PostAdmin(SummernoteModelAdmin):
-
-    list_display = ('title', 'slug', 'status')
-    search_fields = ['title']
-    list_filter = ('status',)
-    prepopulated_fields = {'slug'}
-    summernote_fields = ('content',)
-
-# Register your models here.
-admin.site.register(Comment)
