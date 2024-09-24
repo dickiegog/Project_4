67d5fba5897465a2e97081bbdd199b21a3f8d91f
Richard Goggin <richardgoggin@gmail.com>
2024-09-24 21:38:57 +0000

    Add Authentication

--- my_project/urls.py
+++ my_project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),  # Admin path comes first to prevent slug match
     path('summernote/', include('django_summernote.urls')),  
     path('', include('my_blog.urls')),  # my_blog URLs come after admin and other fixed paths
